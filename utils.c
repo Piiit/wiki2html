a6a2d4677ebeaf156c3ea8bb1d478b107c54a1a6
Peter Moser <pemoser@mint>
2014-01-17 11:49:41 +0100

    utils.c

---
+++ utils.c
@@ -0,0 +1,23 @@
+
+/*
+ * Thx to http://stackoverflow.com/users/9530/adam-rosenfield
+ */
+char *trim(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace(*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace(*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
