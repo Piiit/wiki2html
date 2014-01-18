2b02d1a20702d1662ac5ab5d24e2ab52a1124be9
Peter Moser <pemoser@mint>
2014-01-18 11:48:21 +0100

    runtests.sh supports parameters now, -v for verbose

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+
+function showHelp {
+	echo "Command: $0 [OPTIONS]... [FILES]..."
+	echo "Compiles wiki2html and runs test-cases for it, compares the output of a method with an expected-output file."
+	echo "Returns SUCCEEDED for outputs that are equal to the expected file content."
+	echo "Needed: A input file (<something>.txt) and an expected output file (<something>_expected.txt)."
+	echo
+	echo "OPTIONS:"
+	echo "  -h, --help          Show this help message"
+	echo "  -v, --verbose       Prints additional information, if a test fails"
+}
+
+OPTIND=1
+VERBOSE=0
+
+while getopts "hv" key
+do
+	case $key in
+		h)
+			showHelp	
+			exit 0
+		;;
+		v)
+			VERBOSE=1
+		;;
+		*)
+			echo "'$0 -h' gives you more information."
+			exit 1
+		;;
+	esac
+done
+
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
 clear
 make clean >&2
 make >&2
@@ -10,10 +45,13 @@ do
 		if [ -f "${ARG%.*}_expected.txt" ]; then 
 			echo ============================================================================= >&2
 			echo TESTING: $ARG >&2
-			./wiki < "$ARG" 2>/dev/null | diff -Bbc ${ARG%.*}_expected.txt - >&2
+			./wiki < "$ARG" 2>/tmp/wiki2html_tests_debug | diff -Bbc ${ARG%.*}_expected.txt - >&2
 			if [ $? -eq 0 ]; then
 				echo "TEST SUCCEEDED: $ARG"
 			else
+				if [ $VERBOSE -eq 1 ]; then
+					cat	/tmp/wiki2html_tests_debug
+				fi
 				echo "TEST FAILED: $ARG"
 			fi
 			echo ============================================================================= >&2
